4c0ced3833f420a482a31c44dfaae9b510b92b96
BrunoBaroncelli <80220011+BrunoBaroncelli@users.noreply.github.com>
2021-03-27 09:16:18 -0300

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         <Loading />
       ) : (
         <div className="container">
-          <FaHome size={30} onClick={() => (window.location.href = "https://nextjs-metalhead-8jcsk6za7-brunobaroncelli.vercel.app")}/>
+          <FaHome size={30} onClick={() => (window.location.href = "https://nextjs-metalhead.vercel.app")}/>
           <S.Form onSubmit={handleSubmit(handleLogin)}>
             <S.BoxLogin>
               <S.BoxInput>
